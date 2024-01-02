ecad09c82c97728c32cbd26e1446fd65489f96ed
mouaddigoug <mouaddigoug12@gmail.com>
2024-01-02 19:19:27 +0100

    02-12-24

--- src/middlewares/seller.middleware.ts
+++ src/middlewares/seller.middleware.ts
@@ -14,16 +14,19 @@ const sellerMiddleware = async (req: RequestWithUser, res: Response, next: NextF
 
   try {
     var isVerifiedUser = await isVerifiedSession.executeRead(
-      tx => tx.run("match (u:user {id: $userId})-(s:seller) return u, s", {
+      tx => tx.run("match (u:user {id: $userId})-[:IS_A]-(s:seller) return u, s", {
         userId: userId
       })
     )
+
+    console.log(isVerifiedUser.records.map(record => record.get("u").properties.verified)[0]);
+    
   
     if (isVerifiedUser.records.map(record => record.get("s")).length > 0) {
       if(isVerifiedUser.records.map(record => record.get("s").properties.isVerified)[0]){
         next();
       } else {
-        next(new HttpException(401, 'this user is not verified yet'));
+        next(new HttpException(400, 'this user is not verified yet'));
       }
     } else {
       next(new HttpException(400, 'this user is not a seller'));
