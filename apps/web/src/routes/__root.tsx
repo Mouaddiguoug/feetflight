0ce899e623bdc542aae4fc4ceee002011866dcb4
aymane aallaoui <aymanepraxe@gmail.com>
2025-11-30 21:49:20 +0100

    fix: update comment for type error handling in RootDocument component

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -106,7 +106,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             Deferred
           </Link>{' '}
           <Link
-            // @ts-expect-error
+            //@ts-expect-error - lol
             to="/this-route-does-not-exist"
             activeProps={{
               className: 'font-bold',
